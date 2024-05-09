c02c005dbc1e31d76093f5aca00d0446c93b86c2
sam43516 <kzhirik8@mail.ru>
2024-05-09 16:05:40 +0300

    first commit

---
+++ lesson1/lesson_1_task_6.py
@@ -0,0 +1,32 @@
+def number_1():
+    print("1", end="")
+def number_2():
+    print("2", end="")
+def number_3():
+    print("3", end="")
+def number_4():
+    print("4", end="")
+def number_5():
+    print("5", end="")
+def number_6():
+    print("6", end="")
+def number_7():
+    print("7", end="")
+def number_8():
+    print("8", end="")
+def number_9():
+    print("9", end="")
+def number_0():
+    print("0", end="")
+
+number_8()
+number_8()
+number_0()
+number_0()
+number_5()
+number_5()
+number_5()
+number_3()
+number_5()
+number_3()
+number_5()
